15adb882c6cb580fe54d9b8591df839cc7a1b060
perico16 <70814397+perico16@users.noreply.github.com>
2020-09-12 20:52:45 +0200

    Update signIn.js

--- jojoserie/sign in/signIn.js
+++ jojoserie/sign in/signIn.js
@@ -1,12 +1,16 @@
 
 function contraseñacorrecta() {
         
-    var contraseña = document.getElementById("pass").value;
-    if(contraseña.length<8){
+    var contraseña = document.getElementById("pass");
+    if(contraseña.value.length<8 && contraseña.value.length>0){
         
         document.getElementById("error").innerHTML="Tu contraseña debe tener al menos 8 carácteres";
-        
+        contraseña.style.borderColor="red";
+    } else if(contraseña.value.length==0){
+        document.getElementById("error").innerHTML=""; 
+        contraseña.style.borderColor="";
     } else {
         document.getElementById("error").innerHTML=""; 
+        contraseña.style.borderColor=""; 
     }
 }
